2cba41012bd576d2e31967283da94e915b5b00a1
chena6 <chena6@mcmaster.ca>
2016-04-03 14:24:57 -0400

    basically done ForumPage

--- personal_folders/arthur/Deliverable 4/Beerd/app/src/main/java/arthur/beerd/ForumPage.java
+++ personal_folders/arthur/Deliverable 4/Beerd/app/src/main/java/arthur/beerd/ForumPage.java
@@ -1,7 +1,10 @@
 package arthur.beerd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForumPage extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class ForumPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum_page);
+
+        Button home_page = (Button) findViewById (R.id.home_page);
+        Button search_again = (Button) findViewById(R.id.search_again);
+        Button twitter = (Button) findViewById(R.id.twitter);
+        Button facebook = (Button) findViewById(R.id.facebook);
+        Button googlemaps = (Button) findViewById(R.id.googlemaps);
+    }
+
+    public void buttonOnClickHomePage(View v) {
+        Intent GoToHomePage = new Intent(ForumPage.this, HomePage.class);                  //whatever the homepage class is called here
+        startActivity(GoToHomePage);
+    }
+
+    public void buttonOnClickSearchAgain(View v) {
+        Intent GoToSearchPage = new Intent (ForumPage.this, SearchPage.class);            //whatever the searchpage class is called here
+        startActivity(GoToSearchPage);
+    }
+
+    public void buttonOnClickTwitter(View v) {
+        //twitter tingz here
+    }
+
+    public void buttonOnClickFacebook(View v) {
+        //facebook tingz here
+    }
+
+    public void buttonOnClickGoogleMaps(View v) {
+        Intent GoToGoogleMaps = new Intent (ForumPage.this, GoogleMaps.class);             //whatever the googlemaps class is called here
+        startActivity(GoToGoogleMaps);
     }
 }
